e91d0c9824f48c52caebfca6783f5eb1d3a6264e
HikariTakahashi <uj242085@cc.seikei.ac.jp>
2025-05-03 16:42:08 +0900

    fix:calendar.vue

---
+++ components/calendar/Calendar.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="flex-1 grid grid-cols-7 grid-rows-6 gap-2 h-screen">
+    <div
+      v-for="day in calendarDays"
+      :key="day.date"
+      class="flex flex-col items-center border rounded transition-transform duration-200 relative shadow-md"
+    >
+      <div class="flex items-center justify-center pt-3">
+        {{ day.day }}
+      </div>
+    </div>
+  </div>
+</template>
+<script setup>
+defineProps({
+  calendarDays: Array,
+});
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,46 +1,19 @@
 <template>
   <div class="w-full h-full p-4">
-    <!-- ナビゲーション -->
-
-    <div class="flex justify-between items-center mb-4">
-      <h1 class="flex justify-center items-center text-xl font-bold">
-        シンプルなカレンダー
-      </h1>
-
-      <div class="flex items-center gap-x-4">
-        <h2 class="text-xl font-bold">
-          {{ currentYear }}年 {{ currentMonth + 1 }}月
-        </h2>
-        <button
-          @click="nextMonth"
-          class="px-4 py-2 bg-gray-200 rounded hover:bg-gray-300"
-        >
-          次の月
-        </button>
-        <button
-          @click="prevMonth"
-          class="px-4 py-2 bg-gray-200 rounded hover:bg-gray-300"
-        >
-          前の月
-        </button>
-      </div>
-    </div>
-
-    <!-- カレンダーグリッド -->
-    <div class="grid grid-cols-7 gap-2 w-full h-full">
-      <div
-        v-for="day in calendarDays"
-        :key="day.date"
-        class="flex flex-col items-center border rounded p-2"
-      >
-        <div class="font-bold text-sm">{{ day.day }}</div>
-      </div>
-    </div>
+    <CalendarHeader
+      :current-year="currentYear"
+      :current-month="currentMonth"
+      @next-month="nextMonth"
+      @prev-month="prevMonth"
+    />
+    <Calendar :calendar-days="calendarDays" />
   </div>
 </template>
 
 <script setup>
 import { ref } from "vue";
+import CalendarHeader from "@/components/calendar/CalendarHeader.vue";
+import Calendar from "@/components/calendar/Calendar.vue";
 
 const currentYear = ref(0);
 const currentMonth = ref(0);
