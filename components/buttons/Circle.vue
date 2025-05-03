864058c157a08d025e475f35c84aa7a7645d9704
HikariTakahashi <uj242085@cc.seikei.ac.jp>
2025-05-03 16:39:20 +0900

    feat:buttons-circle

---
+++ components/buttons/Circle.vue
@@ -0,0 +1,12 @@
+<template>
+  <button
+    @click="$emit('click')"
+    class="flex items-center justify-center w-12 h-12 border rounded-full hover:bg-gray-200 flex items-center justify-center"
+  >
+    <slot />
+  </button>
+</template>
+
+<script setup>
+defineEmits(["click"]);
+</script>
