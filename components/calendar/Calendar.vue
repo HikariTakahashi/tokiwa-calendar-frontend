fd26991c14b75750a3de4fe79c1c30e8ad1f7f5a
Hikari Takahashi <166400513+HikariTakahashi@users.noreply.github.com>
2025-05-08 00:45:34 +0900

    feat:defineProps/TimeForm.vue

--- components/calendar/Calendar.vue
+++ components/calendar/Calendar.vue
@@ -1,41 +1,64 @@
 <template>
-  <!-- たぶんもっと良いclassの記述がある -->
   <div
-    class="grid grid-cols-7 pb-2 gap-2 flex-1 overflow-y-auto [grid-auto-rows:minmax(7.9rem,_1fr)]"
+    class="grid grid-cols-7 py-2 gap-2 flex-1 overflow-y-auto [grid-auto-rows:minmax(7.5rem,_1fr)]"
   >
     <div
       v-for="date in calendarDays"
       :key="date.date"
       :class="[
-        'flex flex-col items-center border rounded transition-transform duration-200 relative shadow-md',
+        'flex flex-col items-center border rounded transition-transform duration-200 hover:-translate-y-1 relative shadow-md',
         isCurrentMonth(date.date) ? '' : 'bg-gray-200',
       ]"
+      @click="openForm(date.date)"
     >
       <div class="flex items-center justify-center pt-3">
         {{ new Date(date.date).getDate() }}
       </div>
     </div>
   </div>
+
+  <TimeForm
+    v-if="showModal"
+    :close="closeForm"
+    :selectedDate="selectedDate"
+    @save="onSave"
+    @delete="onDelete"
+  />
 </template>
 
 <script setup>
+import TimeForm from "@/components/forms/TimeForm.vue";
+
 const props = defineProps({
-  calendarDays: {
-    type: Array,
-    required: true,
-  },
-  year: {
-    type: Number,
-    required: true,
-  },
-  month: {
-    type: Number,
-    required: true,
-  },
+  calendarDays: Array,
+  year: Number,
+  month: Number,
 });
+
+const emit = defineEmits(["save, delete"]);
+
+const onSave = (data) => {
+  emit("save", data);
+};
+
+const onDelete = (data) => {
+  emit("delete", data);
+};
 
 const isCurrentMonth = (dateString) => {
   const d = new Date(dateString);
   return d.getFullYear() === props.year && d.getMonth() + 1 === props.month;
+};
+
+const showModal = ref(false);
+const selectedDate = ref(null);
+
+const openForm = (date) => {
+  selectedDate.value = date;
+  showModal.value = true;
+};
+
+const closeForm = () => {
+  showModal.value = false;
 };
 </script>
