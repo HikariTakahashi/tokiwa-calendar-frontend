fe8b0e69faf0bb857d62ae87131361e313a15661
Hikari Takahashi <166400513+HikariTakahashi@users.noreply.github.com>
2025-05-11 22:22:19 +0900

    feat/曜日の表示

--- components/calendar/CalendarWeeks.vue
+++ components/calendar/CalendarWeeks.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="flex-1 grid grid-cols-7 justify-center gap-2">
-    <!-- 開始曜日を変更できるよう将来的に変更 -->
+    <!-- 開始曜日・表示形式を変更できるよう将来的に変更 -->
     <div
       v-for="week in ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat']"
       :key="week"
