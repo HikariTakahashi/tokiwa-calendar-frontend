bd037beb2bff84885c120a871b32728d34dada81
Hikari Takahashi <166400513+HikariTakahashi@users.noreply.github.com>
2025-05-16 22:31:27 +0900

    feat/metadata&icon

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,4 +3,29 @@ export default defineNuxtConfig({
   compatibilityDate: "2024-11-01",
   devtools: { enabled: true },
   modules: ["@nuxt/ui", "@nuxt/icon"],
+  app: {
+    head: {
+      title: "TokiWa | シンプルなカレンダーアプリ",
+      htmlAttrs: {
+        lang: "ja",
+      },
+      link: [
+        {
+          rel: "icon",
+          type: "image/x-icon",
+          href: "/favicon.ico",
+        },
+      ],
+      meta: [
+        {
+          name: "description",
+          content: "TokiWa-Calendar | シンプルで使いやすいカレンダーアプリ",
+        },
+        {
+          name: "keywords",
+          content: "カレンダー,予定管理,日程調整,ToDo,日本語,TokiWa",
+        },
+      ],
+    },
+  },
 });
