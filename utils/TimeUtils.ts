5a12ad526fd4c924bc1cd9f3949ee1b115cc92a0
HikariTakahashi <uj242085@cc.seikei.ac.jp>
2025-05-30 10:27:14 +0900

    flat/TimeUtils.ts/responsive

--- utils/TimeUtils.ts
+++ utils/TimeUtils.ts
@@ -1,4 +1,5 @@
 import { ref } from "vue";
+import { useWindowSize } from "@vueuse/core";
 
 export interface TimeSlot {
   start: string;
@@ -6,6 +7,7 @@ export interface TimeSlot {
 }
 
 export const useTimeUtils = () => {
+  const { width } = useWindowSize();
   const timeSlots = ref<TimeSlot[]>([
     {
       start: "",
@@ -30,6 +32,8 @@ export const useTimeUtils = () => {
 
   // 時間の表示テキストを変更(表示用)
   const formatTimeForDisplay = (timeSlots: TimeSlot[]) => {
+    const isMobile = width.value < 640; // sm: 640px未満をモバイルと判定
+
     return timeSlots
       .map((time) => {
         // 開始時刻が00:00かつ終了時刻が00:00の場合
@@ -38,14 +42,16 @@ export const useTimeUtils = () => {
         }
         // 終了時刻が00:00の場合
         else if (time.end === "00:00") {
-          return `${time.start}~終日`;
+          return isMobile ? `(${time.start}\n終日)` : `${time.start}~終日`;
         }
         // 開始時刻が00:00の場合
         else if (time.start === "00:00") {
-          return `~${time.end}`;
+          return isMobile ? `(開始\n${time.end})` : `~${time.end}`;
         }
         // 通常の時間表示
-        return `${time.start}~${time.end}`;
+        return isMobile
+          ? `(${time.start}\n${time.end})`
+          : `${time.start}~${time.end}`;
       })
       .join("\n");
   };
