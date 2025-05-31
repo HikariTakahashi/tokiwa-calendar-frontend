ad3fe4968075dd0e25100a22a00d8f4ed1fbf74a
Hikari Takahashi <166400513+HikariTakahashi@users.noreply.github.com>
2025-05-31 10:15:21 +0900

    fix/CalendarWeeks.vue/css

--- components/calendar/CalendarWeeks.vue
+++ components/calendar/CalendarWeeks.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="grid grid-cols-7 gap-0.5 sm:gap-2 sm:p-1.5">
+  <div class="grid grid-cols-7 gap-0.5 sm:gap-2 sm:px-1.5">
     <!-- 開始曜日・表示形式を変更できるよう将来的に変更 -->
     <div
       v-for="week in ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat']"
