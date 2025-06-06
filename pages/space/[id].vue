c278d4a7c8e6b008004a5845c50309d5539101c0
HikariTakahashi <uj242085@cc.seikei.ac.jp>
2025-06-06 13:25:36 +0900

    feat/[id].vue/TimeForm.vue

--- pages/space/[id].vue
+++ pages/space/[id].vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="container p-2 max-w-full max-h-full">
+  <div class="container p-2 max-w-full h-screen flex flex-col">
     <div
       class="flex flex-col sm:flex-row justify-between items-center py-2 sx:py-1 px-2"
     >
@@ -33,12 +33,13 @@
 
     <CalendarWeeks />
 
-    <div class="grid grid-cols-7 gap-2">
+    <div class="grid grid-cols-7 gap-2 flex-1">
       <div
         v-for="date in calendarDays"
         :key="date.date"
-        class="flex flex-col items-center border rounded transition-transform duration-200 hover:-translate-y-1 shadow-md min-h-[100px]"
+        class="flex flex-col items-center border rounded transition-transform duration-200 hover:-translate-y-1 shadow-md min-h-[100px] cursor-pointer"
         :class="[isCurrentMonth(date.date) ? '' : 'bg-gray-100']"
+        @click="openTimeForm(date.date)"
       >
         <div
           class="flex items-center justify-center pt-2"
@@ -64,6 +65,16 @@
       :timeData="timeData"
       @close="showUploadForm = false"
     />
+
+    <IDsTimeForm
+      v-if="showTimeForm"
+      :is-open="showTimeForm"
+      :selected-date="selectedDate"
+      :initial-time-slots="getTimeSlots(selectedDate)"
+      @close="closeTimeForm"
+      @save="saveTimeData"
+      @delete="deleteTimeData"
+    />
   </div>
 </template>
 
@@ -72,6 +83,7 @@ import { ref, onMounted, computed } from "vue";
 import { useRoute } from "vue-router";
 import { useTimeUtils } from "@/utils/TimeUtils";
 import type { TimeSlot } from "@/utils/TimeUtils";
+import IDsTimeForm from "@/components/ids/IDsTimeForm.vue";
 
 interface CalendarDay {
   date: string;
@@ -91,6 +103,9 @@ const { formatTimeForDisplay } = useTimeUtils();
 const currentDate = ref(new Date());
 const currentYear = computed(() => currentDate.value.getFullYear());
 const currentMonth = computed(() => currentDate.value.getMonth() + 1);
+
+const showTimeForm = ref(false);
+const selectedDate = ref("");
 
 const prevMonth = () => {
   currentDate.value = new Date(
@@ -112,6 +127,23 @@ const nextMonth = () => {
 
 const openUploadForm = () => {
   showUploadForm.value = true;
+};
+
+const openTimeForm = (date: string) => {
+  selectedDate.value = date;
+  showTimeForm.value = true;
+};
+
+const closeTimeForm = () => {
+  showTimeForm.value = false;
+};
+
+const saveTimeData = (date: string, slots: TimeSlot[]) => {
+  timeData.value[date] = slots;
+};
+
+const deleteTimeData = (date: string) => {
+  delete timeData.value[date];
 };
 
 const getTimeSlots = (date: string): TimeSlot[] => {
