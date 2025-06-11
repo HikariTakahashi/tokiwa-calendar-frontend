2d30d67357508f7c6418c6f8f2bb057d71213181
Hikari Takahashi <166400513+HikariTakahashi@users.noreply.github.com>
2025-06-11 23:57:31 +0900

    feat/ColorButton.vue

---
+++ components/buttons/ColorButton.vue
@@ -0,0 +1,17 @@
+<template>
+  <button
+    class="w-6 h-6 rounded border"
+    :style="{ backgroundColor: color }"
+    @click="emit('update:modelValue', color)"
+  ></button>
+</template>
+
+<script setup lang="ts">
+defineProps<{
+  color: string;
+}>();
+
+const emit = defineEmits<{
+  "update:modelValue": [value: string];
+}>();
+</script>
