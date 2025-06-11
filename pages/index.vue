5e3a439c6f8d6757e96e3ea8a5ea0ae277ebedc2
Hikari Takahashi <166400513+HikariTakahashi@users.noreply.github.com>
2025-06-12 00:10:26 +0900

    refacter/index.vue/date-logic

--- pages/index.vue
+++ pages/index.vue
@@ -13,28 +13,32 @@
       @cancel-copy-mode="handleCancelCopyMode"
     />
     <CalendarWeeks />
-    <div class="flex-1 overflow-auto sx:py-1">
-      <Calendar
-        :calendar-days="calendarDays"
-        :year="currentYear"
-        :month="currentMonth"
-        :is-copy-mode="isCopyMode"
-        @save="saveTime"
-        @delete="deleteTime"
-        @update:time-data="updateTimeData"
-        @update:is-copy-mode="updateIsCopyMode"
-        @cancel-copy-mode="handleCancelCopyMode"
-      />
-    </div>
+    <Calendar
+      :calendar-days="calendarDays"
+      :year="currentYear"
+      :month="currentMonth"
+      :is-copy-mode="isCopyMode"
+      :space-id="spaceId"
+      @save="saveTime"
+      @delete="deleteTime"
+      @update:time-data="updateTimeData"
+      @update:is-copy-mode="updateIsCopyMode"
+      @cancel-copy-mode="handleCancelCopyMode"
+    />
   </div>
 </template>
 
-<script setup>
+<script setup lang="ts">
 import { ref } from "vue";
 import CalendarHeader from "@/components/calendar/CalendarHeader.vue";
 import CopyModeHeader from "@/components/calendar/CopyModeHeader.vue";
 import Calendar from "@/components/calendar/Calendar.vue";
 import { useDateUtils } from "@/utils/DateUtils";
+import type { TimeSlot } from "@/utils/TimeUtils";
+
+interface TimeData {
+  [key: string]: TimeSlot | TimeSlot[];
+}
 
 const {
   currentYear,
@@ -45,17 +49,19 @@ const {
   nextMonth,
   prevMonth,
 } = useDateUtils();
-const timeData = ref({});
+
+const timeData = ref<TimeData>({});
 const isCopyMode = ref(false);
+const spaceId = ref<string | undefined>(undefined);
 const calendarDays = ref(
   getCalendarDays(currentYear.value, currentMonth.value)
 );
 
-const updateTimeData = (newTimeData) => {
+const updateTimeData = (newTimeData: TimeData) => {
   timeData.value = newTimeData;
 };
 
-const updateIsCopyMode = (value) => {
+const updateIsCopyMode = (value: boolean) => {
   isCopyMode.value = value;
 };
 
@@ -73,12 +79,18 @@ const handlePrevMonth = () => {
   calendarDays.value = getCalendarDays(currentYear.value, currentMonth.value);
 };
 
-const saveTime = ({ date, timeSlots }) => {
+const saveTime = ({
+  date,
+  timeSlots,
+}: {
+  date: string;
+  timeSlots: TimeSlot[];
+}) => {
   timeData.value[date] = timeSlots;
 };
 
-const deleteTime = (date) => {
-  delete timeData.value[date];
+const deleteTime = (data: { date: string }) => {
+  delete timeData.value[data.date];
 };
 
 const handleCancelCopyMode = () => {
