b18d0c05bce35dd0f544914115e5cf18eead7d8d
HikariTakahashi <uj242085@cc.seikei.ac.jp>
2025-07-01 16:07:53 +0900

    feat/NavigationSection.vue

---
+++ components/welcome/sections/NavigationSection.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="flex flex-col w-1/4 pt-10">
+    <button
+      @click="navigateTo('/welcome/getting-started')"
+      class="flex flex-row items-center w-full gap-x-2 pl-6 py-2 hover:underline hover:text-blue-500"
+    >
+      <UIcon name="ic:baseline-rocket-launch" class="w-5 h-5 text-gray-500" />
+      <p>Introduction</p>
+    </button>
+    <button
+      @click="navigateTo('/welcome/getting-started/entering-events')"
+      class="flex flex-row items-center w-5/6 gap-x-2 pl-6 py-2 hover:underline hover:text-blue-500"
+    >
+      <UIcon name="ic:baseline-edit-calendar" class="w-5 h-5 text-gray-500" />
+      <p>予定の入力</p>
+    </button>
+    <button
+      @click="navigateTo('/welcome/getting-started')"
+      class="flex flex-row items-center w-5/6 gap-x-2 pl-6 py-2 hover:underline hover:text-blue-500"
+    >
+      <UIcon name="mdi:calendar-blank-multiple" class="w-5 h-5 text-gray-500" />
+      <p>コピーモード</p>
+    </button>
+    <button
+      @click="navigateTo('/welcome/getting-started')"
+      class="flex flex-row items-center w-5/6 gap-x-2 pl-6 py-2 hover:underline hover:text-blue-500"
+    >
+      <UIcon name="ic:baseline-content-copy" class="w-5 h-5 text-gray-500" />
+      <p>入力した予定のコピー</p>
+    </button>
+    <button
+      @click="navigateTo('/welcome/getting-started')"
+      class="flex flex-row items-center w-3/4 gap-x-2 pl-6 py-2 hover:underline hover:text-blue-500"
+    >
+      <UIcon name="ic:baseline-share" class="w-5 h-5 text-gray-500" />
+      <p>カレンダーの共有</p>
+      <h2
+        class="font-bold text-xs bg-blue-500 rounded-sm px-1.5 py-0.5 text-white font-mono"
+      >
+        Beta
+      </h2>
+    </button>
+  </div>
+</template>
+
+<script setup lang="ts">
+// UIconは親でグローバル登録されている前提
+</script>
