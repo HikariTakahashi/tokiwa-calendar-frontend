77a8674e268e5665ab2f47ef580d9c77d312ccd5
HikariTakahashi <uj242085@cc.seikei.ac.jp>
2025-07-01 16:24:11 +0900

    update/NavigationSection.vue/url

--- components/welcome/sections/NavigationSection.vue
+++ components/welcome/sections/NavigationSection.vue
@@ -15,21 +15,21 @@
       <p>予定の入力</p>
     </button>
     <button
-      @click="navigateTo('/welcome/getting-started')"
+      @click="navigateTo('/welcome/getting-started/copy-mode')"
       class="flex flex-row items-center w-5/6 gap-x-2 pl-6 py-2 hover:underline hover:text-blue-500"
     >
       <UIcon name="mdi:calendar-blank-multiple" class="w-5 h-5 text-gray-500" />
       <p>コピーモード</p>
     </button>
     <button
-      @click="navigateTo('/welcome/getting-started')"
+      @click="navigateTo('/welcome/getting-started/data-copy')"
       class="flex flex-row items-center w-5/6 gap-x-2 pl-6 py-2 hover:underline hover:text-blue-500"
     >
       <UIcon name="ic:baseline-content-copy" class="w-5 h-5 text-gray-500" />
       <p>入力した予定のコピー</p>
     </button>
     <button
-      @click="navigateTo('/welcome/getting-started')"
+      @click="navigateTo('/welcome/getting-started/data-share')"
       class="flex flex-row items-center w-3/4 gap-x-2 pl-6 py-2 hover:underline hover:text-blue-500"
     >
       <UIcon name="ic:baseline-share" class="w-5 h-5 text-gray-500" />
