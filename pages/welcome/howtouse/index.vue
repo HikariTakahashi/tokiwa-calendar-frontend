a8d5443c29d620800dd5390f1691ca2066a81e4d
HikariTakahashi <uj242085@cc.seikei.ac.jp>
2025-07-01 09:34:06 +0900

    feat/howtouse-index.vue

---
+++ pages/welcome/howtouse/index.vue
@@ -0,0 +1,12 @@
+<template>
+  <welcome-header />
+  <div class="flex justify-center">
+    <div class="flex flex-col">
+      <h2 class="text-3xl sm:text-4xl font-bold pt-16 sm:pt-10">使い方</h2>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import WelcomeHeader from "~/components/header/WelcomeHeader.vue";
+</script>
