fbab019f3ce25c1bbd5c15a4ee1ee22e34721f41
HikariTakahashi <uj242085@cc.seikei.ac.jp>
2025-07-05 13:16:52 +0900

    feat/password-reset-index.vue

---
+++ pages/login/password-reset/index.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="bg-white bg-opacity-0 sm:bg-opacity-80">
+    <WelcomeHeader />
+  </div>
+  <div class="flex flex-col h-screen mb-[-50px]">
+    <NightSky class="z-[-10]" :seed="456" />
+    <div class="absolute inset-0 z-50 flex items-center justify-center p-4">
+      <div
+        class="bg-white rounded-lg shadow-xl w-full max-w-sm max-h-[98vh] sm:max-h-[80vh] overflow-y-auto"
+      >
+        <div class="p-6">
+          <div class="flex flex-row items-center justify-between mb-2">
+            <h3 class="text-lg font-bold text-gray-800">パスワードリセット</h3>
+          </div>
+          <div class="flex flex-col gap-y-4">
+            リセット機能は現在開発中です。
+          </div>
+          <div class="flex flex-col gap-y-4">
+            担当者にお問い合わせください。
+          </div>
+          <div class="flex flex-col gap-y-4">担当者名: Hikari / おおにし</div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <Footer />
+</template>
+
+<script setup lang="ts">
+import WelcomeHeader from "@/components/header/WelcomeHeader.vue";
+import NightSky from "@/components/background/NightSky.vue";
+import Footer from "@/components/footer/Footer.vue";
+</script>
