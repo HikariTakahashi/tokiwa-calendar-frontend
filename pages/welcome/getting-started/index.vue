a062d4a0d138d0b00f74864088c5b18ea7157d39
yuttto0024 <yuto0024@outlook.com>
2025-07-06 23:14:51 +0900

    update:introduction

--- pages/welcome/getting-started/index.vue
+++ pages/welcome/getting-started/index.vue
@@ -7,58 +7,49 @@
   <div class="flex flex-row w-full pt-10">
     <NavigationSection />
     <div
-      class="flex flex-col w-5/6 h-auto min-h-screen py-6 sm:px-10 border-l-2 border-gray-200"
+      class="flex flex-col w-5/6 h-auto min-h-screen py-12 sm:px-16 border-l-2 border-gray-200"
     >
       <div class="flex flex-col gap-y-2">
-        <h1 class="pl-2 text-3xl sm:text-4xl font-bold">Introduction</h1>
-        <h3 class="pl-4 text-sm text-gray-500">
-          このページでは、カレンダーアプリ「Tokiwa
+        <h1 class="text-3xl sm:text-4xl font-bold">Introduction</h1>
+        <p class="text-sm text-gray-500">
+          このページでは、カレンダーアプリ「TokiWa
           Calendar」の使い方を紹介します。
-        </h3>
+        </p>
       </div>
-      <div class="flex w-full h-0.5 bg-gray-200 my-10" />
 
-      <div class="flex flex-col gap-y-12 px-4">
+      <div class="w-full h-0.5 bg-gray-200 my-12" />
+
+      <div class="flex flex-col gap-y-16">
         <div class="flex flex-col gap-y-4">
-          <h2 class="text-3xl font-bold">
-            日程調整：Tokiwaが実現する、スマートなスケジュール管理
-          </h2>
-          <p class="text-base text-gray-700 leading-relaxed pt-2">
-            あなたは、友人と遊びに行く時、チームでプロジェクトを進める時、あるいは重要なオンライン会議を設定する時、どのように日程調整をしていますか？
-            <br />
-            Tokiwaの日程調整モードは、そうした一手間を解消し、あなたのスケジュール管理をサポートします。
-          </p>
-
-          <div class="flex flex-col gap-y-3 pt-4">
-            <h3 class="text-xl font-bold border-b-2 border-blue-500 pb-2">
-              簡単入力で即座に共有
-            </h3>
-            <p class="text-base text-gray-700 leading-relaxed pt-2">
-              直感的なカレンダースタイルのインターフェースで、希望の時間を簡単に入力できます。入力した日程は、同期ボタンひとつでオンラインカレンダーに即座に反映され、あなた専用のURLが発行されます。このURLを共有するだけで、世界中のどこからでも、あなたの空き状況を誰でも確認できるようになります。
-            </p>
+          <div class="flex flex-row items-center gap-x-4">
+            <span class="text-4xl font-bold text-blue-800">01</span>
+            <h2 class="text-3xl font-bold">日程調整</h2>
           </div>
-
-          <div class="flex flex-col gap-y-3 pt-4">
-            <h3 class="text-xl font-bold border-b-2 border-blue-500 pb-2">
-              チームのスケジュールを一元管理
-            </h3>
-            <p class="text-base text-gray-700 leading-relaxed pt-2">
-              さらに、新しいカレンダーを作成すれば、チーム専用のURLが生成されます。これにより、メンバー全体の動ける時間を一元的に管理することが可能になります。キックオフミーティングやプロジェクトのタスク管理など、あらゆるシーンでチーム全体の連携を強力にサポートし、プロジェクトの進行を加速させます。
+          <div class="flex flex-col pl-14 gap-y-5">
+            <h3 class="text-xl font-semibold">チームの予定も、これ一つで</h3>
+            <div class="w-full h-[1px] bg-gray-400" />
+            <p class="text-base text-gray-700 leading-relaxed">
+              Tokia
+              Calendarは、個人からチームまで、あらゆる日程調整をスムーズにするためのアプリです。あなたの空き時間を共有URLとして発行し、簡単なスケジュール調整を実現します。さらにチームで利用すれば、メンバーの予定も一元管理が可能です。
             </p>
           </div>
         </div>
 
-        <div id="task-management" class="flex flex-col gap-y-4 pt-8">
-          <h2 class="text-3xl font-bold">タスク管理</h2>
-          <div
-            class="flex flex-col items-center justify-center bg-gray-100 p-10 rounded-lg"
-          >
-            <h3 class="text-4xl font-bold font-mono text-gray-500">
-              Coming Soon...
-            </h3>
-            <p class="text-sm text-gray-600 mt-4">
-              タスク管理モードは、2025.08.28にリリース予定です。
-            </p>
+        <div class="flex flex-col gap-y-4">
+          <div class="flex flex-row items-center gap-x-4">
+            <span class="text-4xl font-bold text-blue-800">02</span>
+            <h2 class="text-3xl font-bold">タスク管理</h2>
+          </div>
+          <div class="flex flex-col pl-14 gap-y-5">
+            <div class="w-full h-[1px] bg-gray-400" />
+            <div
+              class="flex flex-col items-center justify-center bg-gray-100 rounded-lg p-12 mt-4"
+            >
+              <h4 class="text-4xl font-bold text-gray-400">Coming Soon...</h4>
+              <p class="text-sm text-gray-500 mt-2">
+                タスク管理モードは、2025.08.28にリリース予定です。
+              </p>
+            </div>
           </div>
         </div>
       </div>
