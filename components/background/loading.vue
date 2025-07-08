28cff4dc5031533c746653fc4b1c49ea57c3faec
HikariTakahashi <uj242085@cc.seikei.ac.jp>
2025-07-08 09:52:52 +0900

    feat/loading.vue

---
+++ components/background/loading.vue
@@ -0,0 +1,14 @@
+<template>
+  <div
+    class="fixed inset-0 bg-white bg-opacity-90 flex items-center justify-center z-50"
+  >
+    <div class="text-center">
+      <UIcon name="svg-spinners:ring-resize" class="size-12" />
+      <p class="text-gray-600 text-lg font-medium">データを読み込み中...</p>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+// ローディング画面のコンポーネント
+</script>
