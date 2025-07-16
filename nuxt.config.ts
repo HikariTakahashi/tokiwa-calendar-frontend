9e76dc534746b136d7d0b18562f60465c9b2eb07
HikariTakahashi <uj242085@cc.seikei.ac.jp>
2025-07-16 11:09:08 +0900

    update/nuxt.config.ts/disable-darkmode

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,7 @@ export default defineNuxtConfig({
   compatibilityDate: "2024-11-01",
   devtools: { enabled: true },
   modules: ["@nuxt/ui", "@nuxt/icon"],
+  css: ["~/assets/css/light-mode-only.css"],
   runtimeConfig: {
     public: {
       apiBaseUrl: process.env.NUXT_PUBLIC_API_BASE_URL,
